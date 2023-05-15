6b0e82fca42ea1fd613506f4c8f10d2014a8db69
SOHTA0318 <nicosohta0318@gmail.com>
2023-05-15 14:40:05 -0400

    lesson39 null undefined object value

--- sec6 Redis has gotacha!/sandbox/index.ts
+++ sec6 Redis has gotacha!/sandbox/index.ts
@@ -4,7 +4,10 @@ import { client } from '../src/services/redis';
 const run = async () => {
 	await client.hSet('car', {
 		color: 'red',
-		year: 1950
+		year: 1950,
+		engine: { cylinders: 8 }, //* it will be '[object Object]'
+		owner: null || '', //* can't store null null.toString()
+		service: undefined || '' //* same as null
 	});
 
 	const car = await client.hGetAll('car');
