909526fca4179623c991cafd6a730c6a6fbb8853
SOHTA0318 <nicosohta0318@gmail.com>
2023-05-15 14:46:34 -0400

    lesson40 hGet not exist hash

--- sec6 Redis has gotacha!/sandbox/index.ts
+++ sec6 Redis has gotacha!/sandbox/index.ts
@@ -10,7 +10,14 @@ const run = async () => {
 		service: undefined || '' //* same as null
 	});
 
+	//* not exist
+	const notExist = await client.hGetAll('car#123');
 	const car = await client.hGetAll('car');
 	console.log(car);
+	if (Object.keys(notExist).length === 0) {
+		console.log('Car not found, respond with 404');
+		return;
+	}
+	console.log(notExist); //* it will return {}
 };
 run();
